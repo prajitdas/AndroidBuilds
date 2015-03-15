1aa29b8616e96f7e5489b9fdf21f649d90c95947
Prajit Kumar Das <prajitdas@users.noreply.github.com>
2015-03-14 20:53:09 -0400

    Update androidSourceBuild.sh

--- code/androidSourceBuild.sh
+++ code/androidSourceBuild.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ -z $1 ]; then
-	echo "Usage: ./androidSourceBuild.sh $userId $androidBuildBranchInfo\nWe need the user id for setting the permissions."
+	echo "Usage: ./androidSourceBuild.sh userId androidBuildBranchInfo"
+	echo "We need the user id for setting the permissions."
 else
 	if [ -z $2 ]; then
 		echo "We need the branch info"
