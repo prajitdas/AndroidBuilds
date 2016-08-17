f2ce2f5f3fb5a26b94743d70cfb79653a53deeef
Prajit Kumar Das <prajitdas@gmail.com>
2016-08-17 08:17:35 -0400

    new versions added

--- code/androidSetupDl.sh
+++ code/androidSetupDl.sh
@@ -1,90 +1,92 @@
 #!/bin/bash
 if [ -z $1 ]; then
-	echo "Usage: ./androidSourceBuild.sh userId androidBuildBranchInfo"
+	echo "Usage: ./androidSourceBuild.sh userId androidBuildBranchInfo deviceInfo"
 	echo "We need the user id for setting the permissions."
 else
 	if [ -z $2 ]; then
 		echo "We need the branch info. Is it 'android-4.0.1_r1' or something else?"
-	else
-		release=`lsb_release -a | grep Release | cut -f2 -d ":" | tr -d '\t'`
-		echo "	Initializing Build Environment
-				Installing the JDK
-				The master branch of Android in the Android Open Source Project (AOSP) requires Java 7. On Ubuntu, use OpenJDK.
-				Java 7: For the latest version of Android"
-		sudo apt-get update
-		sudo apt-get install openjdk-7-jdk
-		echo "Optionally, update the default Java version by running:"
-		sudo update-alternatives --config java
-		sudo update-alternatives --config javac
-		if [ $release=='12.04' ]; then
-			echo "	Installing required packages (Ubuntu 12.04)
-					You will need a 64-bit version of Ubuntu. Ubuntu 12.04 is recommended. Building using an older version of Ubuntu is not supported on master or recent releases."
-			sudo apt-get install git gnupg flex bison gperf build-essential \
-			zip curl libc6-dev libncurses5-dev:i386 x11proto-core-dev \
-			libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-glx:i386 \
-			libgl1-mesa-dev g++-multilib mingw32 tofrodos \
-			python-markdown libxml2-utils xsltproc zlib1g-dev:i386
-			sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
-		elif [[ $release == *'14.04'* ]]; then
-			echo "	Installing required packages (Ubuntu 14.04)
-					Building on Ubuntu 14.04 is experimental at the moment but will eventually become the recommended environment."
-			sudo apt-get install bison g++-multilib git gperf libxml2-utils zip curl git-core gnupg flex bison gperf build-essential
-		elif [[ $release == *'15.04'* ]]; then
-			echo "	Installing required packages (Ubuntu 15.04)
-					Building on Ubuntu 15.04 is experimental at the moment but will eventually become the recommended environment."
-			sudo apt-get install bison g++-multilib git gperf libxml2-utils zip curl git-core gnupg flex bison gperf build-essential
+		if [ -z $3 ]; then
+			echo "We need the device info. Is it 'hammerhead' or something else?"
 		else
-			echo "	Installing required packages (Ubuntu 10.04 -- 11.10)
-					Building on Ubuntu 10.04-11.10 is no longer supported, but may be useful for building older releases of AOSP."
-			sudo apt-get install git-core gnupg flex bison gperf build-essential \
-			zip curl zlib1g-dev libc6-dev lib32ncurses5-dev ia32-libs \
-			x11proto-core-dev libx11-dev lib32readline5-dev lib32z-dev \
-			libgl1-mesa-dev g++-multilib mingw32 tofrodos python-markdown \
-			libxml2-utils xsltproc
-			if [ $release=='11.10' ]; then
-				sudo apt-get install libx11-dev:i386
+			release=`lsb_release -a | grep Release | cut -f2 -d ":" | tr -d '\t'`
+			echo "	Initializing Build Environment
+					Installing the JDK
+					The master branch of Android in the Android Open Source Project (AOSP) requires Java 7. On Ubuntu, use OpenJDK.
+					Java 7: For the latest version of Android"
+			sudo apt-get update
+			sudo apt-get install openjdk-7-jdk
+			echo "Optionally, update the default Java version by running:"
+			sudo update-alternatives --config java
+			sudo update-alternatives --config javac
+			if [ $release=='12.04' ]; then
+				echo "	Installing required packages (Ubuntu 12.04)
+						You will need a 64-bit version of Ubuntu. Ubuntu 12.04 is recommended. Building using an older version of Ubuntu is not supported on master or recent releases."
+				sudo apt-get install git gnupg flex bison gperf build-essential \
+				zip curl libc6-dev libncurses5-dev:i386 x11proto-core-dev \
+				libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-glx:i386 \
+				libgl1-mesa-dev g++-multilib mingw32 tofrodos \
+				python-markdown libxml2-utils xsltproc zlib1g-dev:i386
+				sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
+			elif [[ $release == *'14.04'* ]]; then
+				echo "	Installing required packages (Ubuntu 14.04)
+						Building on Ubuntu 14.04 is experimental at the moment but will eventually become the recommended environment."
+				sudo apt-get install bison g++-multilib git gperf libxml2-utils zip curl git-core gnupg flex bison gperf build-essential
+			elif [[ $release == *'15.04'* ]]; then
+				echo "	Installing required packages (Ubuntu 15.04)
+						Building on Ubuntu 15.04 is experimental at the moment but will eventually become the recommended environment."
+				sudo apt-get install bison g++-multilib git gperf libxml2-utils zip curl git-core gnupg flex bison gperf build-essential
 			else
-				sudo ln -s /usr/lib32/mesa/libGL.so.1 /usr/lib32/mesa/libGL.so
+				echo "	Installing required packages (Ubuntu 10.04 -- 11.10)
+						Building on Ubuntu 10.04-11.10 is no longer supported, but may be useful for building older releases of AOSP."
+				sudo apt-get install git-core gnupg flex bison gperf build-essential \
+				zip curl zlib1g-dev libc6-dev lib32ncurses5-dev ia32-libs \
+				x11proto-core-dev libx11-dev lib32readline5-dev lib32z-dev \
+				libgl1-mesa-dev g++-multilib mingw32 tofrodos python-markdown \
+				libxml2-utils xsltproc
+				if [ $release=='11.10' ]; then
+					sudo apt-get install libx11-dev:i386
+				else
+					sudo ln -s /usr/lib32/mesa/libGL.so.1 /usr/lib32/mesa/libGL.so
+				fi
 			fi
-		fi
-		echo "	Configuring USB Access
-				Under GNU/linux systems (and specifically under Ubuntu systems), regular users can't directly access USB devices by default. The system needs to be configured to allow such access.
-				The recommended approach is to create a file /etc/udev/rules.d/51-android.rules (as the root user) and to copy the following lines in it. <username> must be replaced by the actual username of the user who is authorized to access the phones over USB."
-		sudo echo '# adb protocol on passion (Nexus One)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4e12", MODE="0600", OWNER="$1"
-		# fastboot protocol on passion (Nexus One)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="0bb4", ATTR{idProduct}=="0fff", MODE="0600", OWNER="$1"
-		# adb protocol on crespo/crespo4g (Nexus S)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4e22", MODE="0600", OWNER="$1"
-		# fastboot protocol on crespo/crespo4g (Nexus S)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4e20", MODE="0600", OWNER="$1"
-		# adb protocol on stingray/wingray (Xoom)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="22b8", ATTR{idProduct}=="70a9", MODE="0600", OWNER="$1"
-		# fastboot protocol on stingray/wingray (Xoom)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="708c", MODE="0600", OWNER="$1"
-		# adb protocol on maguro/toro (Galaxy Nexus)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="04e8", ATTR{idProduct}=="6860", MODE="0600", OWNER="$1"
-		# fastboot protocol on maguro/toro (Galaxy Nexus)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4e30", MODE="0600", OWNER="$1"
-		# adb protocol on panda (PandaBoard)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="0451", ATTR{idProduct}=="d101", MODE="0600", OWNER="$1"
-		# adb protocol on panda (PandaBoard ES)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="d002", MODE="0600", OWNER="$1"
-		# fastboot protocol on panda (PandaBoard)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="0451", ATTR{idProduct}=="d022", MODE="0600", OWNER="$1"
-		# usbboot protocol on panda (PandaBoard)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="0451", ATTR{idProduct}=="d00f", MODE="0600", OWNER="$1"
-		# usbboot protocol on panda (PandaBoard ES)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="0451", ATTR{idProduct}=="d010", MODE="0600", OWNER="$1"
-		# adb protocol on grouper/tilapia (Nexus 7)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4e42", MODE="0600", OWNER="$1"
-		# fastboot protocol on grouper/tilapia (Nexus 7)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4e40", MODE="0600", OWNER="$1"
-		# adb protocol on manta (Nexus 10)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4ee2", MODE="0600", OWNER="$1"
-		# fastboot protocol on manta (Nexus 10)
-		SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4ee0", MODE="0600", OWNER="$1"' > /etc/udev/rules.d/51-android.rules
-		sudo $1
+			echo "	Configuring USB Access
+					Under GNU/linux systems (and specifically under Ubuntu systems), regular users can't directly access USB devices by default. The system needs to be configured to allow such access.
+					The recommended approach is to create a file /etc/udev/rules.d/51-android.rules (as the root user) and to copy the following lines in it. <username> must be replaced by the actual username of the user who is authorized to access the phones over USB."
+			sudo echo '# adb protocol on passion (Nexus One)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4e12", MODE="0600", OWNER="$1"
+			# fastboot protocol on passion (Nexus One)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="0bb4", ATTR{idProduct}=="0fff", MODE="0600", OWNER="$1"
+			# adb protocol on crespo/crespo4g (Nexus S)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4e22", MODE="0600", OWNER="$1"
+			# fastboot protocol on crespo/crespo4g (Nexus S)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4e20", MODE="0600", OWNER="$1"
+			# adb protocol on stingray/wingray (Xoom)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="22b8", ATTR{idProduct}=="70a9", MODE="0600", OWNER="$1"
+			# fastboot protocol on stingray/wingray (Xoom)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="708c", MODE="0600", OWNER="$1"
+			# adb protocol on maguro/toro (Galaxy Nexus)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="04e8", ATTR{idProduct}=="6860", MODE="0600", OWNER="$1"
+			# fastboot protocol on maguro/toro (Galaxy Nexus)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4e30", MODE="0600", OWNER="$1"
+			# adb protocol on panda (PandaBoard)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="0451", ATTR{idProduct}=="d101", MODE="0600", OWNER="$1"
+			# adb protocol on panda (PandaBoard ES)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="d002", MODE="0600", OWNER="$1"
+			# fastboot protocol on panda (PandaBoard)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="0451", ATTR{idProduct}=="d022", MODE="0600", OWNER="$1"
+			# usbboot protocol on panda (PandaBoard)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="0451", ATTR{idProduct}=="d00f", MODE="0600", OWNER="$1"
+			# usbboot protocol on panda (PandaBoard ES)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="0451", ATTR{idProduct}=="d010", MODE="0600", OWNER="$1"
+			# adb protocol on grouper/tilapia (Nexus 7)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4e42", MODE="0600", OWNER="$1"
+			# fastboot protocol on grouper/tilapia (Nexus 7)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4e40", MODE="0600", OWNER="$1"
+			# adb protocol on manta (Nexus 10)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4ee2", MODE="0600", OWNER="$1"
+			# fastboot protocol on manta (Nexus 10)
+			SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4ee0", MODE="0600", OWNER="$1"' > /etc/udev/rules.d/51-android.rules
+			sudo $1
 
 		# echo "	Using a separate output directory
 		#		By default, the output of each build is stored in the out/ subdirectory of the matching source tree.
@@ -99,6 +101,33 @@ else
 		# For build tags llok at https://source.android.com/source/build-numbers.html
 		# Downloading the source
 		# Build	Branch	Version	Supported devices
+		# MXC89L 	android-6.0.1_r63 	Marshmallow 	Pixel C
+		# MTC20F 	android-6.0.1_r62 	Marshmallow 	Nexus 5X, Nexus 6P
+		# MOB30Y 	android-6.0.1_r60 	Marshmallow 	Nexus 5
+		# MOB30X 	android-6.0.1_r59 	Marshmallow 	Nexus 7 (flo/deb)
+		# MOB30W 	android-6.0.1_r58 	Marshmallow 	Nexus 6, Nexus 9 (volantis/volantisg), Nexus Player
+		# MMB30S 	android-6.0.1_r57 	Marshmallow 	Nexus 7 (deb)
+		# MMB30R 	android-6.0.1_r56 	Marshmallow 	Nexus 6
+		# MXC89K 	android-6.0.1_r55 	Marshmallow 	Pixel C
+		# MTC19Z 	android-6.0.1_r54 	Marshmallow 	Nexus 5X
+		# MTC19X 	android-6.0.1_r53 	Marshmallow 	Nexus 6P
+		# MOB30P 	android-6.0.1_r50 	Marshmallow 	Nexus 5, Nexus 7 (flo/deb), Nexus 9 (volantis/volantisg), Nexus Player
+		# MOB30O 	android-6.0.1_r49 	Marshmallow 	Nexus 6
+		# MMB30M 	android-6.0.1_r48 	Marshmallow 	Nexus 7 (deb)
+		# MMB30K 	android-6.0.1_r47 	Marshmallow 	Nexus 6
+		# MOB30M 	android-6.0.1_r46 	Marshmallow 	Nexus 5, Nexus 6, Nexus 7 (flo/deb), Nexus 9 (volantis/volantisg), Nexus Player
+		# MTC19V 	android-6.0.1_r45 	Marshmallow 	Nexus 5X, Nexus 6P
+		# MOB30J 	android-6.0.1_r43 	Marshmallow 	Nexus 7 (flo/deb)
+		# MOB30I 	android-6.0.1_r42 	Marshmallow 	Nexus 6
+		# MOB30H 	android-6.0.1_r41 	Marshmallow 	Nexus 5
+		# MOB30G 	android-6.0.1_r40 	Marshmallow 	Nexus 9 (volantis/volantisg), Nexus Player
+		# MXC89H 	android-6.0.1_r33 	Marshmallow 	Pixel C
+		# MXC89F 	android-6.0.1_r32 	Marshmallow 	Pixel C
+		# MMB30J 	android-6.0.1_r28 	Marshmallow 	Nexus 6, Nexus 7 (deb)
+		# MTC19T 	android-6.0.1_r25 	Marshmallow 	Nexus 5X, Nexus 6P
+		# M5C14J 	android-6.0.1_r31 	Marshmallow 	Pixel C
+		# MOB30D 	android-6.0.1_r30 	Marshmallow 	Nexus 5, Nexus 6, Nexus 7 (flo/deb), Nexus 9 (volantis/volantisg), Nexus Player
+		# MHC19Q 	android-6.0.1_r24 	Marshmallow 	Nexus 5X, Nexus 6P
 		# MHC19J	android-6.0.1_r22	Marshmallow	Nexus 5X
 		# MXC14G	android-6.0.1_r18	Marshmallow	Pixel C
 		# MMB29V	android-6.0.1_r17	Marshmallow	Nexus 5, Nexus 5X, Nexus 6, Nexus 6P, Nexus 7 (flo/deb), Nexus 9 (volantis/volantisg)
@@ -240,29 +269,31 @@ else
 		# DRD20	android-1.6_r1.4		
 		# DRD08	android-1.6_r1.3		
 		# DRC92	android-1.6_r1.2				
-		mkdir ~/bin
-		PATH=~/bin:$PATH
-		sudo curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-		chmod a+x ~/bin/repo
-		cd ../builds/$2
-		repo init -u https://android.googlesource.com/platform/manifest -b $2
-		repo sync
-
-		# echo "	Setting up ccache
-		# 		You can optionally tell the build to use the ccache compilation tool. Ccache acts as a compiler cache that can be used to speed-up rebuilds. This works very well if you do 'make clean' often, or if you frequently switch between different build products.
-		# 		Put the following in your .bashrc or equivalent.
-		# 		By default the cache will be stored in ~/.ccache. If your home directory is on NFS or some other non-local filesystem, you will want to specify the directory in your .bashrc as well.
-		# 		The suggested cache size is 50-100GB. You will need to run the following command once you have downloaded the source code:
-		# 		When building Ice Cream Sandwich (4.0.x) or older, ccache is in a different location:
-		# 		This setting is stored in the CCACHE_DIR and is persistent."
-		# export USE_CCACHE=1
-		# mkdir -p ../builds/$2/ccache
-		# export CCACHE_DIR=../builds/$2/ccache
-		# prebuilts/misc/linux-x86/ccache/ccache -M 50G
+			mkdir ~/bin
+			PATH=~/bin:$PATH
+			sudo curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+			chmod a+x ~/bin/repo
+			mkdir -p ../builds/$3/$2
+			cd ../builds/$3/$2
+			repo init -u https://android.googlesource.com/platform/manifest -b $2
+			repo sync
+	
+			# echo "	Setting up ccache
+			# 		You can optionally tell the build to use the ccache compilation tool. Ccache acts as a compiler cache that can be used to speed-up rebuilds. This works very well if you do 'make clean' often, or if you frequently switch between different build products.
+			# 		Put the following in your .bashrc or equivalent.
+			# 		By default the cache will be stored in ~/.ccache. If your home directory is on NFS or some other non-local filesystem, you will want to specify the directory in your .bashrc as well.
+			# 		The suggested cache size is 50-100GB. You will need to run the following command once you have downloaded the source code:
+			# 		When building Ice Cream Sandwich (4.0.x) or older, ccache is in a different location:
+			# 		This setting is stored in the CCACHE_DIR and is persistent."
+			# export USE_CCACHE=1
+			# mkdir -p ../builds/$2/ccache
+			# export CCACHE_DIR=../builds/$2/ccache
+			# prebuilts/misc/linux-x86/ccache/ccache -M 50G
 		
-		# #Building the system
-		# source build/envsetup.sh
-		# lunch # use aosp_buildtag(mako for example)-eng for engineering build with root access and debugging tools
-		# make -j4 # try to use -j16 for fast builds
+			# #Building the system
+			# source build/envsetup.sh
+			# lunch # use aosp_buildtag(mako for example)-eng for engineering build with root access and debugging tools
+			# make -j4 # try to use -j16 for fast builds
+		fi
 	fi
 fi
