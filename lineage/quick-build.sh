2a6fa53e9d307cd627c63ed96761723ef0292f07
Prajit Kumar Das <prajitdas@users.noreply.github.com>
2017-03-01 15:36:06 -0500

    Update quick-build.sh

--- lineage/quick-build.sh
+++ lineage/quick-build.sh
@@ -4,14 +4,15 @@ device=`adb devices | awk '{if($2 == "device") print $1;}' | tr -d '\n'`
 model=`adb -s $device shell getprop ro.product.model | tr -d '\n'`
 date=`date +%Y%m%d | tr -d '\n'`
 build="mako"
+expected="Nexus 4"
 filename="lineage-14.1-"$date"-UNOFFICIAL-"$build".zip"
-if [[ $model == 'Nexus 4' ]]; then
+if [[ $model == $expected ]]; then
         #Measure execution time
         start_time=`date +%s`
         make clobber
         source build/envsetup.sh
         breakfast $build
-        cd ~/android/$build-lineage-14.1-experimental/device/lge/$build
+        cd ~/android/$build-lineage-14.1-experimental-prajit/device/lge/$build
         ./extract-files.sh
         export USE_CCACHE=1
         croot
@@ -26,5 +27,5 @@ if [[ $model == 'Nexus 4' ]]; then
         end_time=`date +%s`
         echo execution time was `expr $end_time - $start_time` s.
 else
-        echo "You have your $model device connected to the computer. This is a build process for the Nexus 4. Please connect your Nexus 4 device to the computer and restart the build."
+        echo "You have your $model device connected to the computer. This is a build process for the $expected. Please connect your $expected device to the computer and restart the build."
 fi
